7a8ddbbaa3a073e76620308000a827da15be65bc
cuom1999 <lephuocdinh99@gmail.com>
2019-03-03 15:04:44 -0600

    Fix move code

--- Bot Code/Move/Move.java
+++ Bot Code/Move/Move.java
@@ -9,21 +9,29 @@ public class Move {
 	
 	public static void runForward(int time) {
 		int speedLeft = 325; 
-		int speedRight = -90;
+		int speedRight = -100;
 		
 		robot.runTwoPCAMotor(CHANNEL_LEFT_WHEEL, speedLeft, CHANNEL_RIGHT_WHEEL, speedRight, time);
 	}
 	
+	public static void upSlope(int time) {
+		int speedLeft = 500; 
+		int speedRight = -350;
+		
+		robot.runTwoPCAMotor(CHANNEL_LEFT_WHEEL, speedLeft, CHANNEL_RIGHT_WHEEL, speedRight, time);
+	}
+	
+	//720 for 90 deg
 	public static void turnLeft(int time) {
-		int speedLeft = -60;
-		int speedRight = -60;
+		int speedLeft = -28;
+		int speedRight = -28;
 		
 		robot.runTwoPCAMotor(CHANNEL_LEFT_WHEEL, speedLeft, CHANNEL_RIGHT_WHEEL, speedRight, time);
 	}
 	
 	public static void turnRight(int time) {
-		int speedLeft = 260;
-		int speedRight = 260;
+		int speedLeft = 258;
+		int speedRight = 258;
 		
 		robot.runTwoPCAMotor(CHANNEL_LEFT_WHEEL, speedLeft, CHANNEL_RIGHT_WHEEL, speedRight, time);
 	}
@@ -34,7 +42,11 @@ public class Move {
 
 		//runForward(3000);
 		
-		//turnLeft(2000);
+		//turnLeft(720);
+		//
+		robot.sleep(3000);
+		turnRight(720);
+		
 		
 		robot.close();
 	}
